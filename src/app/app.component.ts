d3579764ff18316cb23e81c9feb2b5b5fe9f0592
mill <ph.mill@gmail.com>
2019-11-01 11:54:00 +0100

    todd ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,66 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <h1 class='app'>
-      {{ title }}
-    </h1>
-    <h1 [innerHtml]='title'></h1>
-    <img [height]='50' [width]='50' [src]= 'logo'>
-    <div>
-
-      <button (click)=handleButton() >Change Name</button>
-      <input
-        type='text'
-        [value]= 'name'
-        (input)= handleInput($event)
-        (blur)= handleBlur($event)
-      >
-      {{ name }}
-      <input
-        type='text'
-        [ngModel]= 'name2'
-        (ngModelChange)= handleChange($event)
-      >
-      {{ name2 }}
-      <input
-        type='text'
-        [(ngModel)]= 'name2'
-      >
-      {{ name2 }}
-    </div>
-    <div>
-      <button (click)=handleClick(username.value) >Get Username</button>
-      <input
+    <input
       type='text'
-      #username
+      [value]= name
+      (input)= handleChange($event.target.value)
     >
-    {{ name3 }}
+    <ng-template [ngIf]= 'name.length > 2'>
+      <div >
+        Searching for... {{ name }}
+      </div>
+    </ng-template>
+
+    <div *ngIf= name.length>
+      Searching for... {{ name }}
     </div>
   `
 })
 export class AppComponent {
-  title: string;
-  logo: string = 'assets/mill-svgrepo-com.svg';
-  name: string = 'en slip sur internet';
-  name2: string = 'Zbygnew';
-  name3: string = 'Juventus';
-  constructor() {
-    this.title = 'ta mere en slip';
-  }
-  handleButton() {
-    this.name = 'Ta mere '.concat(this.name);
-  }
-  handleInput(event: any) {
-    this.name = event.target.value;
-    console.log(event);
-  }
-  handleBlur(event: any) {
-    this.name = event.target.value;
-  }
+  name: string = '';
+
   handleChange(value: string) {
-    this.name2 = value;
-  }
-  handleClick(value: string) {
-    this.name3 = value;
+    this.name = value;
   }
 }
